45f552059bede60c926e3bb8f8d45d8d417aadd0
Ogoigbe1 <ogoigbe12@gmail.com>
2024-11-08 12:17:55 +0100

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react' 
+
+export default function Index () {
+  return (
+    <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    }} >
+      <Text>Edit app/layout in src folder</Text>
+    </View>
+  )
+}
