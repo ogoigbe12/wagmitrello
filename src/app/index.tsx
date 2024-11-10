04d222494d4a53b9575306ba7568e9fe6300fa8d
Ogoigbe1 <ogoigbe12@gmail.com>
2024-11-10 10:14:35 +0100

    added new packages and updated env

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index () {
         alignItems: 'center',
 
     }} >
-      <Text>Edit app/layout in src folder</Text>
+      <Text>Hello supabase and clerk</Text>
     </View>
   )
 }
---
+++ src/components/AuthModel.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AuthModel = () => {
+  return (
+    <View>
+      <Text>AuthModel</Text>
+    </View>
+  )
+}
+
+export default AuthModel
